d02e20ff6c61a07c3422a5e289bb7951055c671e
Jude Quintana <jude.quintana@gmail.com>
2025-05-25 13:24:15 -0600

    remove dup

--- providers.tf
+++ providers.tf
@@ -3,11 +3,4 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# pull region from aws provider
-data "aws_region" "current" {}
-
-locals {
-  region = data.aws_region.current.name
-}
-
 provider "random" {}
