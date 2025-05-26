4aa5c2e1179a3e9cf41e2ec47368157d21cf9417
Jude Quintana <jude.quintana@gmail.com>
2025-05-26 10:16:06 -0600

    moar fluid naming

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -1,7 +1,11 @@
 # use secrets manager to access admin creds elsewhere
 # ie rds/env/mysql/app
+locals {
+  secrets_manager_name_path = format("rds/%s/%s", var.env_prefix, "mysql/app")
+}
+
 resource "aws_secretsmanager_secret" "rds" {
-  name = format("rds/%s/%s", var.env_prefix, "mysql/app")
+  name = local.secrets_manager_name_path
 }
 
 resource "aws_secretsmanager_secret_version" "rds" {
