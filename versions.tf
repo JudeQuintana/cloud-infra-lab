ea9d2ba5e187d48a5e4c05dd9c8169623cbf1010
Jude Quintana <jude.quintana@gmail.com>
2025-08-18 16:32:03 -0600

    provider comment

--- versions.tf
+++ versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_version = "~>1.4"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
+      # the AWS 6.9+ provider version will also work here but will show deprecations that havent been updated
       version = "~>5.100"
     }
     random = {
