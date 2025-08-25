c887cfaa280e6c9abcf5d84c78eec3503be3badf
Jude Quintana <jude.quintana@gmail.com>
2025-08-24 18:29:32 -0600

    rds module docs

--- modules/rds/outputs.tf
+++ modules/rds/outputs.tf
@@ -9,3 +9,9 @@ output "primary_address" {
 output "read_replica_address" {
   value = aws_db_instance.this_read_replica.address
 }
+
+# not same as family for db_parameter group
+# for use with rds_proxy default will be "MYSQL"
+output "engine_family" {
+  value = contains(["mysql"], var.rds.engine) ? upper(var.rds.engine) : null
+}
