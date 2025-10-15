1c0b6595da8b973f4ff30e8f8047e1afa08ff243
Jude Quintana <jude.quintana@gmail.com>
2025-10-15 16:18:41 -0600

    reorg

--- modules/ipam/variables.tf
+++ modules/ipam/variables.tf
@@ -11,6 +11,15 @@ variable "ipam" {
     operating_regions = optional(list(string), [])
     provision_cidrs   = list(string)
   })
+
+  # using ipv4 validation via cidrnetmask function instead of regex for ipv4
+  # caches most bad CIDR notations
+  validation {
+    condition = alltrue(flatten([
+      for this in var.ipam.provision_cidrs : can(cidrnetmask(this))
+    ]))
+    error_message = "All CIDRs in var.ipam.provision_cidrs must be in valid IPv4 CIDR notation (ie x.x.x.x/xx -> 10.46.0.0/20). Check for typos."
+  }
 }
 
 variable "tags" {
