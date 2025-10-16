1d6af64ed72bd83b3800df4e23c7e3d8f0709930
Jude Quintana <jude.quintana@gmail.com>
2025-10-16 13:41:42 -0600

    description

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "zone_name" {
 }
 
 variable "enable_ipam" {
-  description = "Must decide to wether or not to toggle to enable IPAM module prior to first apply or use existing IPAM pools."
+  description = "Must decide to wether or not to toggle to enable IPAM module prior to first apply or use existing IPAM pools via description lookup."
   type        = bool
   default     = false
 }
