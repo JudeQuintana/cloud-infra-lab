058c85dd220dc220bb36eccabead5ce6bc2bdcf9
Jude Quintana <jude.quintana@gmail.com>
2025-10-16 13:59:51 -0600

    comments

--- modules/ipam/outputs.tf
+++ modules/ipam/outputs.tf
@@ -6,10 +6,9 @@ output "ipv4_pool" {
   # dont output entire object only needed attributes
   value = {
     id = aws_vpc_ipam_pool.this_ipv4.id
-    # not expecting provision_cidrs to be consumed by a tiered vpc (only the ipv4_pool.id) but using this ipv4_pool object to force specific DAG behavior
+    # not expecting provision_cidrs to be consumed by a tiered vpc (only the ipv4_pool.id is needed) by populating the provision cidrs in the ipv4_pool object to force specific DAG behavior
     # this is a shim/hack forcing the DAG to finish provisioning the module cidrs before the pool id is consumed without needing the consuming vpc to have a direct/explicit dependency on the ipam module aws_vpc_ipam_pool_cidr.this_ipv4 (not possible).
     # otherwise the consumer vpc of the pool id will try to use the ipam cidr before the expected cidr is finished being provisioned (race condition)
-    # using pool cidrs resource and not using pass thru var.ipam.cidrs
     provision_cidrs = [for this in aws_vpc_ipam_pool_cidr.this_ipv4 : this.cidr]
   }
 }
